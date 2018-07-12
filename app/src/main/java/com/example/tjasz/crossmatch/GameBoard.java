a1e3f9d76be35f1e7db28897b5afb0b5f66e52d2
tjasz <nukorausa@hotmail.com>
2018-07-11 22:24:02 -0700

    Add a GridView of Buttons corresponding to GameBoard size

--- app/src/main/java/com/example/tjasz/crossmatch/GameBoard.java
+++ app/src/main/java/com/example/tjasz/crossmatch/GameBoard.java
@@ -1,4 +1,30 @@
 package com.example.tjasz.crossmatch;
 
 public class GameBoard {
+    public GameBoard()
+    {
+        set_size(default_size);
+    }
+    public GameBoard(int size)
+    {
+        set_size(size);
+    }
+
+    // manage the size of the square game board
+    private static final int default_size = 6;
+    private static int size_;
+    public static int size()
+    {
+        return size_;
+    }
+    public static void set_size(int new_size)
+    {
+        if (new_size > 0) {
+            size_ = new_size;
+        }
+        else
+        {
+            throw new RuntimeException("An attempt to set a non-positive board size was observed.");
+        }
+    }
 }
--- app/src/main/java/com/example/tjasz/crossmatch/MainActivity.java
+++ app/src/main/java/com/example/tjasz/crossmatch/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.tjasz.crossmatch;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GridView gameboard_gridview;
+
+    private GameBoard game_board;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        game_board = new GameBoard();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gameboard_gridview = findViewById(R.id.gameboard_gridview);
+        gameboard_gridview.setNumColumns(game_board.size());
+        gameboard_gridview.setAdapter(new ButtonAdapter(this));
+    }
+
+    // http://www.stealthcopter.com/blog/2010/09/android-creating-a-custom-adapter-for-gridview-buttonadapter/
+    public class ButtonAdapter extends BaseAdapter {
+        private Context mContext;
+
+        // Gets the context so it can be used later
+        public ButtonAdapter(Context c) {
+            mContext = c;
+        }
+
+        // Total number of things contained within the adapter
+        public int getCount() {
+            return GameBoard.size() * GameBoard.size();
+        }
+
+        // Require for structure, not really used in my code.
+        public Object getItem(int position) {
+            return null;
+        }
+
+        // Require for structure, not really used in my code. Can
+        // be used to get the id of an item in the adapter for
+        // manual control.
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position,
+                            View convertView, ViewGroup parent) {
+            Button btn;
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                btn = new Button(mContext);
+                btn.setPadding(8, 8, 8, 8);
+            }
+            else {
+                btn = (Button) convertView;
+            }
+            btn.setText("?");
+            // filenames is an array of strings
+            btn.setTextColor(Color.WHITE);
+            btn.setId(position);
+
+            return btn;
+        }
     }
 }
