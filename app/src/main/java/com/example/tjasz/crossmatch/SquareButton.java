9752382371344f576b330926f335fdbb025a0c40
tjasz <nukorausa@hotmail.com>
2018-09-13 07:17:50 -0700

    Auto size text in buttons

--- app/src/main/java/com/example/tjasz/crossmatch/SquareButton.java
+++ app/src/main/java/com/example/tjasz/crossmatch/SquareButton.java
@@ -2,19 +2,28 @@ package com.example.tjasz.crossmatch;
 
 // https://stackoverflow.com/questions/24416847/how-to-force-gridview-to-generate-square-cells-in-android
 import android.content.Context;
+import android.support.v4.widget.TextViewCompat;
 import android.util.AttributeSet;
 
 public class SquareButton extends android.support.v7.widget.AppCompatButton {
     public SquareButton(Context context) {
         super(context);
+        setUpAutoSizing();
     }
 
     public SquareButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setUpAutoSizing();
     }
 
     public SquareButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        setUpAutoSizing();
+    }
+
+    private void setUpAutoSizing()
+    {
+        TextViewCompat.setAutoSizeTextTypeWithDefaults(this, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
     }
 
     @Override
