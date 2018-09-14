b1d6fe40eb00473aa00f7e2d1d0855ffc66ad314
tjasz <nukorausa@hotmail.com>
2018-09-13 19:54:06 -0700

    Add draft HelpActivity

--- app/src/main/java/com/example/tjasz/crossmatch/GameActivity.java
+++ app/src/main/java/com/example/tjasz/crossmatch/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.tjasz.crossmatch;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -34,6 +36,29 @@ public class GameActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_game, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_edit_settings) {
+            // TODO launch settings activity and create mechanism for retrieving settings
+            // Intent intent = new Intent(GameActivity.this, SettingsActivity.class);
+            // startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_help) {
+            Intent intent = new Intent(GameActivity.this, HelpActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
---
+++ app/src/main/java/com/example/tjasz/crossmatch/HelpActivity.java
@@ -0,0 +1,29 @@
+package com.example.tjasz.crossmatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
