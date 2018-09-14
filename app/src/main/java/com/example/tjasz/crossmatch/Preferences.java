2a0f43ab3b7667ff85d7a7840ec764922baa3e0a
tjasz <nukorausa@hotmail.com>
2018-09-13 21:28:47 -0700

    Finish preferences manager

--- app/src/main/java/com/example/tjasz/crossmatch/Preferences.java
+++ app/src/main/java/com/example/tjasz/crossmatch/Preferences.java
@@ -15,21 +15,52 @@ public class Preferences {
     private static final int default_opponent_decision_time = 2000;
     private static final boolean default_use_ai = true;
 
+    private static SharedPreferences get_overall(Context context)
+    {
+        return context.getSharedPreferences(overall_key, MODE_PRIVATE);
+    }
+
+    private static SharedPreferences.Editor get_editor(Context context)
+    {
+        return get_overall(context).edit();
+    }
+
     public static int get_board_size(Context context)
     {
-        SharedPreferences preferences = context.getSharedPreferences(overall_key, MODE_PRIVATE);
+        SharedPreferences preferences = get_overall(context);
         return preferences.getInt(board_size_key, default_board_size);
+    }
+
+    public static void set_board_size(Context context, int size)
+    {
+        SharedPreferences.Editor editor = get_editor(context);
+        editor.putInt(board_size_key, size);
+        editor.commit();
     }
 
     public static int get_opponent_decision_time(Context context)
     {
-        SharedPreferences preferences = context.getSharedPreferences(overall_key, MODE_PRIVATE);
+        SharedPreferences preferences = get_overall(context);
         return preferences.getInt(opponent_decision_time_key, default_opponent_decision_time);
+    }
+
+    public static void set_opponent_decision_time(Context context, int decision_time)
+    {
+        SharedPreferences.Editor editor = get_editor(context);
+        editor.putInt(opponent_decision_time_key, decision_time);
+        editor.commit();
     }
 
     public static boolean get_use_computer_opponent(Context context)
     {
-        SharedPreferences preferences = context.getSharedPreferences(overall_key, MODE_PRIVATE);
-        return preferences.getBoolean(opponent_decision_time_key, default_use_ai);
+        SharedPreferences preferences = get_overall(context);
+        return preferences.getBoolean(use_ai_key, default_use_ai);
+    }
+
+    public static void set_use_computer_opponent(Context context, boolean use_ai)
+    {
+        SharedPreferences.Editor editor = get_editor(context);
+        editor.putBoolean(use_ai_key, use_ai);
+        editor.commit();
     }
 }
